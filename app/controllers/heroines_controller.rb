0860cd2ca0687591966a15f8c84ff67c82426e57
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-04-12 10:23:27 -0700

    creates new and show actions and views for heroines and powers

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,30 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    # byebug
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.save
+      redirect_to heroines_path
+    else
+      render :new
+    end
+    # redirect_to heroines_path
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
