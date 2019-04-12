1797bba6e1ff2074ae1b1282054f3c051fa943cb
quinncidences <qrc.inpa@gmail.com>
2019-04-12 10:14:04 -0700

    Finished all non-bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,23 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    redirect_to heroine_path(@heroine)
+  end
+
+  private
+  def heroine_params
+    params.require(:heroine).permit!
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  resources :powers
+  resources :heroines
 end
+
+
+# , only: [:index]
