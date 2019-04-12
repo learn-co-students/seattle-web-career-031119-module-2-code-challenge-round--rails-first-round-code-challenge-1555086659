fa9691ddb3572a63d34d2115b4cf66a046f4314b
jyang81 <joe.yang.design@gmail.com>
2019-04-12 10:39:58 -0700

    Nailed it.

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,6 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+  validates :name, presence: true
+  validates :super_name, presence: true, uniqueness: true
+  validates :power_id, presence: true
 end
