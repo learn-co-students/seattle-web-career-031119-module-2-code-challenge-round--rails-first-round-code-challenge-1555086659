a9963fd92b7d38719ff9492a15fe49448054d115
github username <github email address>
2019-04-12 10:07:05 -0700

    done without bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,27 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_attributes)
+    if @heroine.save
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def heroine_attributes
+    params.require(:heroine).permit!
+  end
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,4 @@
 class Heroine < ApplicationRecord
+  validates :super_name, uniqueness: true
+  belongs_to :power
 end
